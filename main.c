79ea76f21a2effc8b72d6614fb7e56f2c13d9abe
moskensoap <141073988+moskensoap@users.noreply.github.com>
2024-04-23 12:03:00 +0000

    new file:   .gitignore 	new file:   main.c 	new file:   makefile

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    size_t total_length = 0;
+    size_t mingw_length = strlen("mingw32-make.exe ") + 1;
+
+    if(argc==1){
+        return system("mingw32-make.exe");
+    }
+
+    for (int i = 1; i < argc; i++) {
+        total_length += strlen(argv[i]) + 1;
+    }
+
+    char merged_string[total_length + mingw_length];
+    char *ptr = merged_string;
+
+    strcpy(ptr, "mingw32-make.exe ");
+    ptr += strlen("mingw32-make.exe ");
+
+    for (int i = 1; i < argc; i++) {
+        strcpy(ptr, argv[i]);
+        ptr += strlen(argv[i]);
+        if (i < argc - 1) {
+            *ptr++ = ' ';
+        }
+    }
+
+    *ptr = '\0';
+
+    return system(merged_string);
+}
---
+++ makefile
@@ -0,0 +1,7 @@
+cc=gcc
+cflags=-O3 -s
+
+make.exe:main.c
+	$(cc) main.c -o make.exe $(cflags)
+clean:
+	rm make.exe
